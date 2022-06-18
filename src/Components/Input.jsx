bdaaf9be8bb6a8e9628f5ae0d0f2164e4296fedc
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-06-18 12:24:38 +0530

    added container for gif

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -5,18 +5,42 @@ const Div = styled.div`
 `;
 const Input = () => {
   const [textdata, setTextdata] = React.useState("");
+  const [display, setDisplay] = React.useState(false);
+  const [gifsearch, setGifsearch] = React.useState("");
   const handleChange = (e) => {
     setTextdata(e.target.value);
   };
   const handleAdd = () => {
     console.log(textdata);
   };
-  const handlegif = () => {};
+  const handlegif = () => {
+    display ? setDisplay(false) : setDisplay(true);
+  };
+  const handlegifchange = (e) => {
+    console.log(e.target.value);
+    setGifsearch(e.target.value);
+  };
   return (
     <Div>
       <button onClick={handlegif}>GIF</button>
+
       <input type="text" onChange={handleChange} value={textdata} />
       <button onClick={handleAdd}>Send</button>
+      {display ? (
+        <div
+          style={{
+            margin: "auto",
+            border: "1px solid red",
+            height: "400px",
+            maxHeight: "400px",
+            maxWidth: "300px",
+            backgroundColor: "white",
+            overflowy: "scroll",
+          }}
+        >
+          <input onChange={handlegifchange} value={gifsearch} />
+        </div>
+      ) : null}
     </Div>
   );
 };
