76dd536b3f370759bc6fc4baa272e2581d49dc94
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-06-18 15:21:46 +0530

    added gif

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -7,6 +7,7 @@ const Input = () => {
   const [textdata, setTextdata] = React.useState("");
   const [display, setDisplay] = React.useState(false);
   const [gifsearch, setGifsearch] = React.useState("");
+  const [gif, setGif] = React.useState([]);
   const handleChange = (e) => {
     setTextdata(e.target.value);
   };
@@ -17,9 +18,18 @@ const Input = () => {
     display ? setDisplay(false) : setDisplay(true);
   };
   const handlegifchange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     setGifsearch(e.target.value);
+    getgif();
   };
+  const getgif = () => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=wHbnWRoiesIzNZyKdJcTK1C6V39RpCc7&q=${gifsearch}&limit=25&offset=0&rating=g&lang=en`
+    )
+      .then((res) => res.json())
+      .then((res) => setGif(res.data));
+  };
+
   return (
     <Div>
       <button onClick={handlegif}>GIF</button>
@@ -35,10 +45,19 @@ const Input = () => {
             maxHeight: "400px",
             maxWidth: "300px",
             backgroundColor: "white",
-            overflowy: "scroll",
+            overflowY: "scroll",
           }}
         >
           <input onChange={handlegifchange} value={gifsearch} />
+          {/* {console.log(gif[0].images.downsized_still.url)} */}
+          {gif.map((each) => (
+            <img
+              src={each.images.downsized_still.url}
+              width="250px"
+              height="250px"
+              alt="nothing"
+            />
+          ))}
         </div>
       ) : null}
     </Div>
